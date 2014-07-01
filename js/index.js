63b303fa5d14d5b81eee0b18cb730910a43c3789
Simone Vellei <simone.vellei@inim.biz>
2014-07-01 16:57:42 +0200

    fix

--- js/index.js
+++ js/index.js
@@ -16,6 +16,42 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+    // The watch id references the current `watchAcceleration`
+    var watchID = null;
+
+    function startWatch() {
+
+        // Update acceleration every 3 seconds
+        var options = { frequency: 3000 };
+
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+    }
+
+    // Stop watching the acceleration
+    //
+    function stopWatch() {
+        if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        var element = document.getElementById('accelerometer');
+        element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                            'Acceleration Y: ' + acceleration.y + '<br />' +
+                            'Acceleration Z: ' + acceleration.z + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />';
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    }
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,6 +70,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        startWatch();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
